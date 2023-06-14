faf49d2d14a8d44da7cc5866f1dc92b51980fd72
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2023-06-14 20:14:16 +0200

    version 1.5 /added sql file

--- user.sql
+++ user.sql
@@ -12,20 +12,12 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `login_db`
 --
 
 -- --------------------------------------------------------
 
---
--- Struktura tabeli dla tabeli `user`
---
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
@@ -34,19 +26,6 @@ CREATE TABLE `user` (
   `password_hash` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `name`, `email`, `password_hash`) VALUES
-(1, 'kacper', 'kackar87@wp.pl', '$2y$10$hJ.bWVZHfUTBmmBKX/epUOZYpu.AQGnarykoBh6uEwdsy7.y8jgpq'),
-(4, 'alaniendo', 'radarada@wp.pl', '$2y$10$YXvnMUo6VK/I21rZsIDMLOPVP60oYVF7NxVKoHAJyZYGrwVf.q/ky'),
-(7, 'test', 'test@wp.pl', '$2y$10$NSQZIrxaGyhpyDbL.6aYzezqOyVMuoGkmlAngNRrVAYOVoLGwbUAy'),
-(8, 'admin', 'admin@wp.pl', '$2y$10$kNBVI9y0k1xBdKFw0s87Ru7sdavGc4mGi6YVGtbnnVj.8/rGjt3o.');
-
---
--- Indeksy dla zrzutów tabel
---
 
 --
 -- Indeksy dla tabeli `user`
@@ -56,16 +35,9 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT for dumped tables
---
-
---
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
