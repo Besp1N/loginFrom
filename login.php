7d0eac10dda5ba6b2add6887c0f368f345072afe
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2023-06-14 19:41:59 +0200

    version 1.2 /added working login php

--- login.php
+++ login.php
@@ -1,3 +1,25 @@
+<?php
+
+    $is_invalid = false;
+
+    if($_SERVER["REQUEST_METHOD"] === "POST"){
+        $mysqli = require __DIR__ . "/database.php";
+        $sql = sprintf("SELECT * FROM user
+                        WHERE email = '%s'",
+                        $mysqli->real_escape_string($_POST["email"]));
+
+        $result = $mysqli->query($sql);
+        $user = $result->fetch_assoc();
+        
+        if($user){
+           if( password_verify($_POST["password"], $user["password_hash"])){
+             die("Login successful");
+           }
+        }
+        $is_invalid = true;
+    } 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -8,8 +30,13 @@
   </head>
   <body>
     <h1>Login</h1>
+
+    <?php if($is_invalid): ?>
+        <em>Invalid Login</em>
+    <?php endif; ?>
+    
     <div class="glass">
-        <form>
+        <form method="post">
           <div class="field glass">
             <input type="email" id="email" name="email" placeholder="email"/>
           </div>
