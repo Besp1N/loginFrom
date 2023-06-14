50ef5aa266e1d933af57ee9e6f4d238becd14b18
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2023-06-14 19:18:55 +0200

    version 1.1 /added home page and login form

--- process-signup.php
+++ process-signup.php
@@ -19,5 +19,29 @@
     die("password must match");
   }
   $password_hash = password_hash($_POST["password"],PASSWORD_DEFAULT);
-  var_dump($password_hash);
+
+
+  $mysqli = require __DIR__ . "/database.php";
+  $sql = "INSERT INTO user(name,email,password_hash) VALUES(?,?,?)";
+  $stmt = $mysqli->stmt_init();
+
+  if(! $stmt->prepare($sql)){
+    die("SQL error: " . $mysqli->error);
+  }
+
+  $stmt->bind_param("sss",$_POST["name"],$_POST["email"],$password_hash);
+
+  if($stmt->execute()){
+    header("Location: signup-success.html");
+  }
+  else{
+    if($mysqli->errno ===1062)
+    {
+      die("email already taken:(");
+    } else {
+    die($mysqli->error . " " . $mysqli->errno);
+    }
+  }
+
+
 ?>
